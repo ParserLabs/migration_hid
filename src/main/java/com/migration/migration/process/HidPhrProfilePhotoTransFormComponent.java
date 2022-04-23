91499d63fc9ee2076f2cff7c8ad7ae1c9f44d7e9
ameyaanaikcoredge <amey@coredge>
2022-04-23 17:25:54 +0530

    added the component

--- src/main/java/com/migration/migration/process/HidPhrProfilePhotoTransFormComponent.java
+++ src/main/java/com/migration/migration/process/HidPhrProfilePhotoTransFormComponent.java
@@ -3,10 +3,13 @@ package com.migration.migration.process;
 import java.util.Objects;
 import java.util.function.Function;
 
+import org.springframework.stereotype.Component;
+
 import com.migration.migration.entity.UserEntity;
 import com.migration.migration.request.PhrRequestPlayLoad;
 import com.migration.migration.request.PhrUpdatePhotoProfileRequest;
 
+@Component
 public class HidPhrProfilePhotoTransFormComponent implements Function<UserEntity, PhrUpdatePhotoProfileRequest> {
 
 	@Override
